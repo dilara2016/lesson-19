ff5174d16c69b6248a2b18683a32025a5645fb1c
dilara2016 <m.123.hunain@gmail.com>
2024-12-15 13:32:37 +0100

    Update list.py

--- list.py
+++ list.py
@@ -1,9 +1,9 @@
 empty_list = []
 print()
-numbers = [1,2,3,4,5]
+numbers = [1,2,3,4,5,6,7,8,9]
 print(numbers)
-triples = [1,2,3] * 3
+triples = [1,2,3,4.5] * 3
 print(triples)
-aList = [100,200,300,400,500]
+aList = [100,200,300,400,500,600]
 aList = aList[::-1]
 print(aList,"\n")
